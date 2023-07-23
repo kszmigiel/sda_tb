123a3c7b406c5eb4f8d745a707266b70e91534c9
kszmigiel <kacper_szmigiel@external.mckinsey.com>
2023-07-23 09:57:28 +0200

    custom admin

--- car_rental/rent/admin.py
+++ car_rental/rent/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import Car, Client, Rental
+
+
+class RentalAdmin(ModelAdmin):
+    ordering = ['-id']
+    list_display = ['id', 'car', 'client', 'start_date', 'end_date']
+    list_display_links = ['id']
+    list_filter = ['car', 'client', 'start_date', 'end_date']
+    search_fields = ['car']
+    fieldsets = (
+        (None, {
+            'fields': ('car', 'client', 'price')
+        }),
+        ('Date', {
+            'fields': ('start_date', 'end_date')
+        }),
+
+    )
+    readonly_fields = ['price']
+
+
+
 
 admin.site.register(Car)
 admin.site.register(Client)
-admin.site.register(Rental)
+admin.site.register(Rental, RentalAdmin)
+#admin.site.register(Rental)
 
